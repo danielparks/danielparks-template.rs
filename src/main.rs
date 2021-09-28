2058114c9a89ab61083ffd75d64ce21bcfdb2d73
Daniel Parks <dp+git@oxidized.org>
2021-09-28 00:14:18 -0700

    Flesh out binary crate. Ignore assertify dependency.

--- src/main.rs
+++ src/main.rs
@@ -7,27 +7,42 @@ use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
 struct Params {
+    #[structopt(short, long, parse(from_occurrences))]
+    verbose: u8,
 }
 
-async fn main() {
-    if let Err(error) = cli(Params::from_args()).await {
-        eprintln!("Error: {:#}", error);
-        exit(1);
-    }
+fn main() {
+    smol::block_on(async {
+        if let Err(error) = cli(Params::from_args()).await {
+            eprintln!("Error: {:#}", error);
+            exit(1);
+        }
+    })
 }
 
 async fn cli(params: Params) -> anyhow::Result<()> {
+    let filter = match params.verbose {
+        3.. => LevelFilter::Trace,
+        2 => LevelFilter::Debug,
+        1 => LevelFilter::Info,
+        0 => LevelFilter::Warn,
+    };
+
     // Configure different logging for a module (sqlx::query here).
     CombinedLogger::init(vec![
         // Default logger
         new_term_logger(
-            LevelFilter::Info,
-            new_logger_config().add_filter_ignore_str("sqlx::query").build(),
+            filter,
+            new_logger_config()
+                .add_filter_ignore_str("sqlx::query")
+                .build(),
         ),
         // Logger for sqlx::query
         new_term_logger(
             LevelFilter::Warn,
-            new_logger_config().add_filter_allow_str("sqlx::query").build(),
+            new_logger_config()
+                .add_filter_allow_str("sqlx::query")
+                .build(),
         ),
     ])
     .unwrap();
