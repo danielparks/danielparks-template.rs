fe3e24cbf8891fe2ac9877daa6967a13ddbddafd
Daniel Parks <dp+git@oxidized.org>
2022-12-11 04:47:31 -0800

    cargo fmt

--- benches/general.rs
+++ benches/general.rs
@@ -7,6 +7,6 @@ use {{crate_name}}::*;
 #[bench]
 fn rot13_basic(bench: &mut test::Bencher) {
     let source = "super secure";
-    bench.iter(|| { rot13(&source) });
+    bench.iter(|| rot13(&source) );
     bench.bytes = source.len() as u64;
 }
