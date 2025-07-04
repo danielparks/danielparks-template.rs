a0ba4d7e95d2f5aa4ec796ac351fda34e89c2d55
Daniel Parks <dp+git@oxidized.org>
2025-07-04 15:34:43 -0700

    `test-template.zsh`: pass parameters to `zsh` instead of `cargo generate`.

--- test-template.zsh
+++ test-template.zsh
@@ -2,13 +2,28 @@
 
 set -e
 
+generate_args=()
+case $1 in
+	--bin)
+		generate_args=(--bin --name foo-bin)
+		shift
+		;;
+	--lib)
+		generate_args=(--lib --name foo-lib)
+		shift
+		;;
+	*)
+		echo 'Expect --bin or --lib as first argument.' >&2
+		exit 1
+esac
+
 export TEMPLATE_DIR=${0:A:h}
 export TEST_DIR=$(mktemp -d)
 cd "$TEST_DIR"
 
-cargo generate --init --path "$TEMPLATE_DIR" "$@"
+cargo generate --init --path "$TEMPLATE_DIR" $generate_args
 
-zsh
+zsh "$@"
 
 cd
 echo -n 'Keep temporary directory "'"$TEST_DIR"'" [yN]? '
