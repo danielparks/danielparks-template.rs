a81509f74e8a1a6cf5896146d15608520a77602c
Daniel Parks <dp+git@oxidized.org>
2025-11-01 12:07:20 -0700

    Lints: fix clippy lint, remove deprecated lint.

--- src/params.rs
+++ src/params.rs
@@ -57,9 +57,10 @@ impl Params {
 }
 
 /// Whether or not to output in color
-#[derive(Clone, Copy, Debug, Eq, PartialEq, clap::ValueEnum)]
+#[derive(Clone, Copy, Debug, Default, Eq, PartialEq, clap::ValueEnum)]
 pub enum ColorChoice {
     /// Output in color when running in a terminal that supports it
+    #[default]
     Auto,
 
     /// Always output in color
@@ -67,12 +68,6 @@ pub enum ColorChoice {
 
     /// Never output in color
     Never,
-}
-
-impl Default for ColorChoice {
-    fn default() -> Self {
-        Self::Auto
-    }
 }
 
 impl From<ColorChoice> for termcolor::ColorChoice {
